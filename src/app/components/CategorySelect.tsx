8fe32583bbe5268f93715f1f093765f3fd385851
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-10-12 20:50:11 +0330

    add select category

--- src/app/components/CategorySelect.tsx
+++ src/app/components/CategorySelect.tsx
@@ -1,14 +1,32 @@
+'use client';
 import React from 'react';
-
-export default async function CategorySelect() {
-  const res = await fetch('http://localhost:3000/api/products', {
-    cache: 'no-store',
-  });
-  const category = await res.json();
+import useSWR from 'swr';
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+export default function CategorySelect() {
+  const { data: products, error } = useSWR(
+    'http://localhost:3000/api/products',
+    fetcher
+  );
+  if (error)
+    return (
+      <select>
+        <option>Error Loading</option>
+      </select>
+    );
+  if (!products)
+    return (
+      <select>
+        <option>loading...</option>
+      </select>
+    );
+  const categories = [...new Set(products.map((p: any) => p.category))];
   return (
-    <select>
-      {category.map((item) => (
-        <option key={item.id}>{item.category}</option>
+    <select className="text-black border rounded px-2 py-1">
+      <option value="">All Categories</option>
+      {categories.map((cat, index) => (
+        <option key={index} value={cat}>
+          {cat}
+        </option>
       ))}
     </select>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,11 @@
+'use client';
 import React from 'react';
 import storely from '@/public/storely.png';
 import Image from 'next/image';
 import CategorySelect from './CategorySelect';
 const Navbar = () => {
   return (
-    <nav className="relative bg-blue-200 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+    <nav className="relative bg-blue-200 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10 text-black">
       <Image src={storely} alt="logo" className="w-20 h-15" />
       <ul>
         <li>Home</li>
