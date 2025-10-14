7c10b12802d5217dda0e7fbf810e4c8812b04923
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-10-14 17:51:07 +0330

    navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -8,20 +8,20 @@ import { FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <nav className="relative bg-blue-100 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10 text-black flex items-center justify-evenly w-full">
-      <Image src={storely} alt="logo" className="w-20 h-15" />
-      <ul className="flex justify-between w-30">
+    <nav className=" bg-blue-100 text-black flex items-center justify-around w-full h-full">
+      <Image src={storely} alt="logo" className="w-20 h-15  cursor-pointer" />
+      <ul className="flex justify-between w-50  cursor-pointer">
         <li>Home</li>
         <li>Products</li>
       </ul>
-      <form action="" className="flex justify-between w-100">
-        <div className="flex justify-between w-50 h-10 items-center bg-indigo-50 p-2 rounded-2xl">
+      <form action="" className="flex justify-between w-[40rem] ">
+        <div className="flex justify-between w-[30rem] h-10 items-center bg-indigo-50 p-2 rounded-2xl">
           <input type="text" placeholder="Search Product" className="" />
-          <FaSearch />
+          <FaSearch className="cursor-pointer text-blue-950" />
         </div>
         <CategorySelect />
       </form>
-      <FaShoppingCart />
+      <FaShoppingCart className="cursor-pointer text-blue-950" />
     </nav>
   );
 };
