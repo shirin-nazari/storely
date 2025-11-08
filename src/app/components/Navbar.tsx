ad4fcd4cff5dc28d280ddf7b6328252c37ed4763
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2025-11-08 16:09:40 +0330

    navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { use, useState } from 'react';
+import React, { useState } from 'react';
 import storely from '@/public/storely.png';
 import Image from 'next/image';
 import CategorySelect from './CategorySelect';
